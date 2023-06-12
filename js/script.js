2c0fc3618aafa50c224033f489eb6eecee1ff8a4
Telvin Varfley <telvinvarfley@gmail.com>
2023-06-12 08:42:00 +0000

    UI change

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-const discoverBtn = document.querySelector('.discover-content')
-const burgerIcon = document.querySelector('.burger-icon')
-discoverBtn.addEventListener('click', myFunction)
-function myFunction() {
-  burgerIcon.classList.toggle("change");
-}
